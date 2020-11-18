cda13f570abb62a117dc00d669b2eb231f84cb48
Lin Liu <LLiu41@slb.com>
2020-11-18 16:48:41 +0800

    feat: show different speed message when car with electric engine

--- OOPractice/Car.cs
+++ OOPractice/Car.cs
@@ -4,7 +4,13 @@
 
     public class Car : Vehicle
     {
+        private string name;
+
         public Car(string name, double speed) : base(name, speed)
+        {
+        }
+
+        public Car(string name, Engine engine) : base(name, engine.GetSpeed())
         {
         }
     }
--- OOPracticeTest/CarTest.cs
+++ OOPracticeTest/CarTest.cs
@@ -18,5 +18,20 @@ namespace OOPracticeTest
             //then
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Should_Show_Message_With_Different_Speed_When_Using_Electric_Engine()
+        {
+            //given
+            string expected = "Cool Car: speed up 25 km/h";
+            Engine engine = new Engine("electric");
+            Car car = new Car("Cool Car", engine);
+
+            //when
+            string actual = car.SpeedUp();
+
+            //then
+            Assert.Equal(expected, actual);
+        }
     }
 }
